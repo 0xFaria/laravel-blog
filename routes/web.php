a8273d1023c4f04ba62cf331f1c074141fc7af5e
0xfaria <luksfaria12@outlook.com>
2022-07-12 18:52:58 -0300

    categories page

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,10 @@ Route::get('/', function () {
     //     );
     // }
 
-    return view("posts", ["posts" => Post::latest()->with("category", "author")->get()]);
+    return view("posts",
+        ["posts" => Post::latest()->with("category", "author")->get(),
+            "categories" => Category::all()
+            ]);
 
 
     // return view('posts', [
@@ -65,12 +68,14 @@ Route::get("posts/{post:slug}", function (Post $post) { // wildcard route  param
 
 Route::get("categories/{category:slug}", function (Category $category) {
     return view("posts", [
-        "posts" => $category->posts->load(["category", "author"])
+        "posts" => $category->posts,
+        "categories" => Category::all()
     ]);
 });
 
 Route::get("authors/{author:username}", function (User $author) {
     return view("posts", [
-        "posts" => $author->posts->load(["category", "author"])
+        "posts" => $author->posts,
+        "categories" => Category::all()
     ]);
 });
